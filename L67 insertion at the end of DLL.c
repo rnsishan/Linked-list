963df80e03814e2842a0307ad42e506c713fc9af
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-12 17:54:10 +0530

    Create L67 insertion at the end of DLL.c

---
+++ L67 insertion at the end of DLL.c
@@ -0,0 +1,72 @@
+//l67 insertion at the end of DLL
+
+//1st to traverse the list
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+        struct node * prev;
+        int data;
+        struct node * next;
+        
+    };
+typedef struct node nd;
+
+nd* addToEmpty(nd* head,int data){
+    nd* temp=malloc(sizeof(nd));
+    
+    temp->prev=0;
+    temp->data=data;
+    temp->next=0;
+    head=temp;
+    return head;
+}
+
+nd* addAtBeg(nd* head,int data){
+    nd* temp=malloc(sizeof(nd));
+    temp->prev=0;
+    temp->data=data;
+    temp->next=0;
+    
+    temp->next=head;
+    head->prev=temp;
+    
+    head=temp;
+    return head;
+}
+
+nd* AddAtEnd(nd* head,int data){
+    nd* temp,*tp;
+    temp=malloc(sizeof(nd));
+    temp->prev=0;
+    temp->data=data;
+    temp->next=0;
+    
+    tp=head;
+    while(tp->next!=0){
+        tp=tp->next;
+    }
+    tp->next=temp;
+    temp->prev=tp;
+    return head;
+    
+}
+    
+int main()
+{
+    nd*head=0;
+    nd* ptr=0;
+    head=addToEmpty(head,45);
+    head= addAtBeg(head,34);
+    head=AddAtEnd(head,9);
+    
+    ptr=head;
+    while(ptr!=0){
+    printf("%d ",ptr->data);
+    ptr=ptr->next;    
+    }
+    return 0;
+}
+
+
+
