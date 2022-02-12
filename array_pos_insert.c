cf655bd3fbeaf35e0a60f83cba99f0b0ed96b51c
rnsishan <96770975+rnsishan@users.noreply.github.com>
2022-02-12 17:46:58 +0530

    Create array_pos_insert.c

---
+++ array_pos_insert.c
@@ -0,0 +1,30 @@
+//array element insert at position
+#include <stdio.h>
+
+void add_at_pos(int arr[],int arr2[],int n,int data,int pos){
+   
+    int i;
+    int index =pos-1;
+    for(i=0;i<index-1;i++)
+        arr2[i]=arr[i];
+        
+    arr2[index]=data;
+    int j;
+    for(i=index+1,j=index;i<n+1,j<n;i++,j++)
+        arr2[i]=arr[j];
+}
+
+
+int main()
+{
+  int arr[]={2,34,21,6,7,8,90,97,67,23,39};
+  int pos=5,data=78,i;
+  int size= sizeof(arr)/sizeof(arr[0]);
+  int arr2[size+1];
+  add_at_pos(arr,arr2,size,data,pos);
+  for(i=0;i<size+1;i++)
+    printf("%d \n",arr2[i]);
+    
+    return 0;
+}
+
